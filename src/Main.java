3b33b1d8ccb98468f7929b186b8600cd69f69ed3
latsa <latsapnev01@mail.ru>
2024-03-10 18:23:34 +0300

    Выполнил все задания!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            checkData("login!", "password", "password");
+        } catch (WrongLoginException e) {
+            e.printStackTrace();
+        } catch (WrongPasswordException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void checkData(String login, String password, String confirmPassword) {
+        if (login.length() > 20) {
+            throw new WrongLoginException("Слишком длинный логин");
+        }
+        if (!login.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongLoginException("Использованы недопустимые символы");
+        }
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Слищком длинный пароль");
+        }
+        if (!password.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongPasswordException("Использованы недопустимые символы");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
     }
 }
